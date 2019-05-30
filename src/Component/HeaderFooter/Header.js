fb463b77aec0b1e2a5df04a80ed626a3b12f215e
dixitvora <dixitvora2020@gmail.com>
2019-05-30 15:39:24 +0530

    Header Footer Added

--- src/Component/HeaderFooter/Header.js
+++ src/Component/HeaderFooter/Header.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { AppBar, Toolbar, Button } from '@material-ui/core'
+import { CityLogo } from '../ui/icons';
+import {Link } from 'react-router-dom'
+
+
+
+
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <AppBar 
+        position = 'fixed' 
+        style = {{
+          background : '#98c5e9',
+          boxShadow:'none',
+          padding : '10px 0',
+          borderButtom : '2px solid #00285c'
+        }}
+      >
+        <Toolbar style={{display:'flex'}}>
+          <div style={{flexGrow:1}}>
+            <div className="header_logo">
+              <CityLogo link ={true} linkTo='/' width ='70px' height='70px' />
+            </div>
+          </div>
+          <Link to='/the_team'>
+            <Button color= 'inherit'>The Team</Button>
+          </Link>  
+          <Link to='/the_matches'>
+            <Button color='inherit'>Matches</Button>
+          </Link>
+        </Toolbar>
+
+      </AppBar>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './Resources/Css/App.css'
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './routes';
 
